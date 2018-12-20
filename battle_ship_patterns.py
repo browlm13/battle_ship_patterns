9b33a1edb7c551458da76556b309f59fef87c99c
LJ Brown <browlm13@gmail.com>
2018-12-20 08:51:21 -0600

    Update battle_ship_patterns.py

--- battle_ship_patterns.py
+++ battle_ship_patterns.py
@@ -254,7 +254,26 @@ def fire_pattern_minimum_requirement( ships, board_dims, target_mean, confidence
 			print(" std: ", max_std)
 
 		if diff <= interval_size:
-			print(" %s +- %s" % (max_mean, interval_size))
+
+			print("\n\nFOUND.")
+
+			print("\n\n %s +- %s" % (max_mean, interval_size))
+			print("diff: ", max_diff)
+			print("range: ", max_range)
+			print(" std: ", max_std)
+
+
+			#
+			# Display Top Pattern
+			#
+
+			print("\n\nPATTERN FOUND:\n")
+			print(max_fire_pattern)
+
+			plt.imshow(max_fire_pattern, cmap='hot', alpha=0.8)
+			plt.show()
+
+
 			return max_fire_pattern, max_mean, max_std
 
 	print("failure to find pattern given criteria")
@@ -390,18 +409,22 @@ if __name__ == "__main__":
 	#
 	# find fire pattern by criteria
 	#
-	TARGET_MEAN = len(SHIP_SIZES)
-	CONFIDENCE = 0.64
-	MAX_ITS = 1000
-	NUM_SHOTS = 50
+	TARGET_MEAN = len(SHIP_SIZES) - 0.25
+	CONFIDENCE = 0.75
+	MAX_ITS = 200
+	NUM_SHOTS = 30
 	NUM_TESTS = 75
 
 	fire_pattern, mean, std = fire_pattern_minimum_requirement( ships, BOARD_DIMS, TARGET_MEAN, confidence=CONFIDENCE, max_its=MAX_ITS, num_trials=NUM_TESTS, num_shots=NUM_SHOTS )
 
-	print("Fire pattern found that meets criteia")
-	print(fire_pattern)
 	print("\n mean: ", mean)
 	print("\n std: ", std)
+
+	print("\n\nBEST PATTERN:\n")
+	print(fire_pattern)
+
+	plt.imshow(fire_pattern, cmap='hot', alpha=0.8)
+	plt.show()
 
 	#
 	# Generate Fire Pattern Search
